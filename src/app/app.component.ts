6cb7cdc89a04f79298229748115e4081d11ca737
Your Name <kennystemper@gmail.com>
2021-08-10 16:00:15 -0400

    AngularTodoList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { NodeWithI18n } from '@angular/compiler';
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
+import { Component, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularToDoLab';
+
+  newTask:Item = {task:"", completed: false};
+
+
+  allItems: Item[] = [
+    {task: 'vacuum', completed: true},
+    {task: 'get mail', completed: true},
+    {task: 'make dinner', completed: false},
+    {task: 'walk the dog', completed: true},
+    {task: 'dust', completed: false}
+  ];
+
+  getItems(): Item[] {
+    let list: Item[] = [];
+    this.allItems.forEach((item: Item) =>{
+      if(item.task !== null){
+
+        list.push(item);
+      }
+    });
+    return list;
+  }
+
+addTask(newForm: NgForm){
+  let newTask = newForm.form.value.newTask;
+  this.allItems.push({task: newTask, completed:false});
 }
+
+completeTask(item:Item){
+    if(item.completed === false)
+    {
+      item.completed = true;
+    }
+    return item.completed;
+  }
+}
+
+
---
+++ src/app/item.ts
@@ -0,0 +1,5 @@
+export interface Item
+ {
+   task: string;
+   completed: boolean;
+ }
